b0a640a77d0668ba19358f74872be23941161402
Chris Wyatt <9978862+chriswyatt1@users.noreply.github.com>
2026-03-21 22:17:16 +0100

    First commit with template

---
+++ nf-conf/src/test/groovy/ecoflow/plugin/NfConfObserverTest.groovy
@@ -0,0 +1,22 @@
+package ecoflow.plugin
+
+import nextflow.Session
+import spock.lang.Specification
+
+/**
+ * Implements a basic factory test
+ *
+ */
+class NfConfObserverTest extends Specification {
+
+    def 'should create the observer instance' () {
+        given:
+        def factory = new NfConfFactory()
+        when:
+        def result = factory.create(Mock(Session))
+        then:
+        result.size() == 1
+        result.first() instanceof NfConfObserver
+    }
+
+}
